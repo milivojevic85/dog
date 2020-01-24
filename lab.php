490f84af279e8e350779280cf8e0aee127608140
milivojevic85 <misagrf@yahoo.com>
2020-01-24 11:48:43 +0100

    lab.php added

---
+++ lab.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Lab</title>
+</head>
+<body>
+<?php 
+require_once('dog.php');
+$lab = new Dog;
+$lab->display_properties();
+?>
+</body>
+</html>
