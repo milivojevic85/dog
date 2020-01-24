31b104d5b0420897252849050f783e0517a0ac40
milivojevic85 <misagrf@yahoo.com>
2020-01-24 17:03:53 +0100

    constructor added

--- dog.php
+++ dog.php
@@ -13,26 +13,39 @@ class Dog
 	private $dog_breed = "no breed";
 	private $dog_color = "no color";
 	private $dog_name = "no name";
+	private $error_message = "??";
+	//---------------------------------Constructor----------------------------------
+	function __construct($value1, $value2, $value3, $value4) {
+		$name_error = $this->set_dog_name($value1) == TRUE ? 'TRUE,' : 'FALSE,';
+		$breed_error = $this->set_dog_breed($value2) == TRUE ? 'TRUE,' : 'FALSE,';
+		$color_error = $this->set_dog_color($value3) == TRUE ? 'TRUE,' : 'FALSE,';
+		$weight_error = $this->set_dog_weight($value4) == TRUE ? 'TRUE' : 'FALSE';
+		$this->error_message = $name_error.$breed_error.$color_error.$weight_error;
+	}
+	//---------------------------------toString----------------------------------
+	public function __toString() {
+		return $this->error_message;
+	}
 	//---------------------------------Set Methods----------------------------------
 	function set_dog_name($value) {
 		$error_message = TRUE;
-		(ctype_alpha($value) && strlen($value) <= 20) ? $this->dog_name = $value : $error_message = FALSE; 
-		return $error_message;
+		(ctype_alpha($value) && strlen($value) <= 20) ? $this->dog_name = $value : $this->error_message = FALSE;
+		return $this->error_message;
 	}
 	function set_dog_weight($value) {
 		$error_message = TRUE;
-		(ctype_digit($value) && ($value > 0 && $value <= 120)) ? $this->dog_weight = $value : $error_message = FALSE; 
-		return $error_message;
+		(ctype_digit($value) && ($value > 0 && $value <= 120)) ? $this->dog_weight = $value : $this->error_message = FALSE;
+		return $this->error_message;
 	}
 	function set_dog_breed($value) {
 		$error_message = TRUE;
-		(ctype_alpha($value) && strlen($value) <= 35) ? $this->dog_breed = $value : $error_message = FALSE; 
-		return $error_message;
+		(ctype_alpha($value) && strlen($value) <= 35) ? $this->dog_breed = $value : $this->error_message = FALSE;
+		return $this->error_message;
 	}
 	function set_dog_color($value) {
 		$error_message = TRUE;
-		(ctype_alpha($value) && strlen($value) <= 15) ? $this->dog_color = $value : $error_message = FALSE; 
-		return $error_message;
+		(ctype_alpha($value) && strlen($value) <= 15) ? $this->dog_color = $value : $this->error_message = FALSE;
+		return $this->error_message;
 	}
 	//---------------------------------Get Methods----------------------------------
 	function get_dog_name() {
--- lab.php
+++ lab.php
@@ -7,8 +7,14 @@
 <body>
 <?php 
 require_once('dog.php');
-$lab = new Dog;
-//---------------------------------Set Properties----------------------------------
+$lab = new Dog('Fred','Lab','Yellow','100');
+list($name_error, $breed_error, $color_error, $weight_error) = explode(',', $lab);
+
+print $name_error == 'TRUE' ? 'Name update successful<br>' : 'Name update not successful<br>';
+print $breed_error == 'TRUE' ? 'Breed update successful<br>' : 'Breed update not successful<br>';
+print $color_error == 'TRUE' ? 'Color update successful<br>' : 'Color update not successful<br>';
+print $weight_error == 'TRUE' ? 'Weight update successful<br>' : 'Weight update not successful<br>';
+//---------------------------------Get Properties----------------------------------
 $dog_error_message = $lab->set_dog_name('Fred');
 print $dog_error_message == TRUE ? 'Name update successful<br>' : 'Name update not successful<br>';
 $dog_error_message = $lab->set_dog_weight(50);
